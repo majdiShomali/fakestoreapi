1f374256906ffd81dfe1dd02bc3bbaf17ad2fb4e
majdiShomali <majdi.shomali.1997@gmail.com>
2023-04-16 23:01:46 +0200

    remove the button

--- app.js
+++ app.js
@@ -18,7 +18,47 @@ fetch('https://fakestoreapi.com/products')
                 json.forEach(element => {
                 let  product_ =new Product(element.title, element.price, element.description, element.image)
                 arrData.push(product_)
+
+
               });
+
+              arrData.map(element => {
+  
+                let sdcard= document.getElementById("student_card");
+              
+                let divContainer = document.createElement('div');
+              
+                let image = document.createElement("img");
+                image.src=`${element.image}`
+            
+                let ul = document.createElement("ul");
+              
+                let li1= document.createElement("li");
+              
+                li1.textContent=`title: ${element.title}`;
+                
+                let li2= document.createElement("li");
+                li2.textContent=`price: ${element.price}`;
+                let li3= document.createElement("li");
+                li3.textContent=`description: ${element.description}`;
+               
+              
+                divContainer.appendChild(image);
+                ul.appendChild(li1);
+                ul.appendChild(li2);
+                // ul.appendChild(li3);
+               
+              
+                divContainer.appendChild(ul);
+                sdcard.appendChild(divContainer);
+              
+              
+              
+              
+              
+              } );
+
+
 
             })
 
@@ -27,49 +67,5 @@ fetch('https://fakestoreapi.com/products')
 
    
 
-let x= document.getElementById("button")
-x.addEventListener("click",function(){
-render()
-})
 
 
-//-------------show data-----------------//
-function render(){
-
-  arrData.map(element => {
-  
-    let sdcard= document.getElementById("student_card");
-  
-    let divContainer = document.createElement('div');
-  
-    let image = document.createElement("img");
-    image.src=`${element.image}`
-
-    let ul = document.createElement("ul");
-  
-    let li1= document.createElement("li");
-  
-    li1.textContent=`title: ${element.title}`;
-    
-    let li2= document.createElement("li");
-    li2.textContent=`price: ${element.price}`;
-    let li3= document.createElement("li");
-    li3.textContent=`description: ${element.description}`;
-   
-  
-    divContainer.appendChild(image);
-    ul.appendChild(li1);
-    ul.appendChild(li2);
-    // ul.appendChild(li3);
-   
-  
-    divContainer.appendChild(ul);
-    sdcard.appendChild(divContainer);
-  
-  
-  
-  
-  
-  } );
-
-}
