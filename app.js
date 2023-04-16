28fc8c45724dc6aa6009ae5006d9edbc01cc6a13
majdiShomali <majdi.shomali.1997@gmail.com>
2023-04-16 22:28:24 +0200

    solve the task

---
+++ app.js
@@ -0,0 +1,75 @@
+
+class Product{
+  constructor(title,price,description,image){
+  this.title=title
+  this.price=price
+  this.description=description
+  this.image=image
+  }
+  
+  
+  }
+
+let arrData=[];
+fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=> {
+
+                json.forEach(element => {
+                let  product_ =new Product(element.title, element.price, element.description, element.image)
+                arrData.push(product_)
+              });
+
+            })
+
+
+
+
+   
+
+let x= document.getElementById("button")
+x.addEventListener("click",function(){
+render()
+})
+
+
+//-------------show data-----------------//
+function render(){
+
+  arrData.map(element => {
+  
+    let sdcard= document.getElementById("student_card");
+  
+    let divContainer = document.createElement('div');
+  
+    let image = document.createElement("img");
+    image.src=`${element.image}`
+
+    let ul = document.createElement("ul");
+  
+    let li1= document.createElement("li");
+  
+    li1.textContent=`title: ${element.title}`;
+    
+    let li2= document.createElement("li");
+    li2.textContent=`price: ${element.price}`;
+    let li3= document.createElement("li");
+    li3.textContent=`description: ${element.description}`;
+   
+  
+    divContainer.appendChild(image);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    // ul.appendChild(li3);
+   
+  
+    divContainer.appendChild(ul);
+    sdcard.appendChild(divContainer);
+  
+  
+  
+  
+  
+  } );
+
+}
